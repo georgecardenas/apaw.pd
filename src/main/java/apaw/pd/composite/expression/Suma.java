0815dbc86adc703f5e5e71d33b1400739c2a4df5
georgecardenas <jorge.cardenasc@alumnos.upm.es>
2016-10-07 19:36:13 +0200

    Eliminados espacios extra

--- src/main/java/apaw/pd/composite/expression/Suma.java
+++ src/main/java/apaw/pd/composite/expression/Suma.java
@@ -5,13 +5,10 @@ public class Suma extends Expresion {
     private Expresion expresion1;
     private Expresion expresion2;
     
-
-    
     public Suma(Expresion expresion1, Expresion expresion2) {
         this.expresion1 = expresion1;
         this.expresion2 = expresion2;
     }
-
 
     @Override
     public int operar() {
