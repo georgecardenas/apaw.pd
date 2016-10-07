f6fa2b4fed63a3d03b2e65aceedd7d5d7f003b54
georgecardenas <jorge.cardenasc@alumnos.upm.es>
2016-10-07 19:34:10 +0200

    Solucionado composite.expresion

---
+++ src/main/java/apaw/pd/composite/expression/Division.java
@@ -0,0 +1,22 @@
+package apaw.pd.composite.expression;
+
+public class Division extends Expresion {
+    
+    private Expresion expresion1;
+    private Expresion expresion2;
+
+    public Division (Expresion expresion1, Expresion expresion2){
+        this.expresion1 = expresion1;
+        this.expresion2 = expresion2;
+    }
+
+    @Override
+    public int operar() {
+        return expresion1.operar()/expresion2.operar();
+    }
+
+    @Override
+    public String toString() {
+        return "(" + expresion1.toString() + "/" + expresion2.toString() + ")";
+    }
+}
