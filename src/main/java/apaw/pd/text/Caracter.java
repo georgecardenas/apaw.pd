7ce7eac0727529e547b472195ad659463a017e24
georgecardenas <jorge.cardenasc@alumnos.upm.es>
2016-10-08 12:20:13 +0200

    Primeras clases

---
+++ src/main/java/apaw/pd/text/Caracter.java
@@ -0,0 +1,28 @@
+package apaw.pd.text;
+
+public class Caracter extends Texto{
+
+    @Override
+    public void add(Texto texto) {
+        throw new UnsupportedOperationException("Ésta operación no etá soportada.");
+    }
+
+    @Override
+    public void remove(Texto texto) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String print() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String printMayuscula() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
---
+++ src/main/java/apaw/pd/text/Parrafo.java
@@ -0,0 +1,29 @@
+package apaw.pd.text;
+
+public class Parrafo extends Texto{
+
+    @Override
+    public void add(Texto texto) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void remove(Texto texto) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String print() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String printMayuscula() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
